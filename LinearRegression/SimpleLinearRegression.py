958ee39ab6b164223c1cf453d4e62b4f01ec84a7
m560h <914892930@qq.com>
2018-06-07 01:55:43 +0800

    Simple Linear Regression using vector

--- LinearRegression/SimpleLinearRegression.py
+++ LinearRegression/SimpleLinearRegression.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class SimpleLinearRegression:
+class SimpleLinearRegression1:
 
     def __init__(self):
         """初始化Simple Linear Regression模型"""
@@ -41,3 +41,51 @@ class SimpleLinearRegression:
     def _predict(self, single_x):
         """给定单个待预测数据single_x，返回single_x的预测结果值"""
         return single_x * self.a_ + self.b_
+
+    def __repr__(self):
+        return "SimpleLinearRegression1"
+
+
+class SimpleLinearRegression2:
+
+    def __init__(self):
+        """初始化Simple Linear Regression模型"""
+        self.a_ = None
+        self.b_ = None
+
+    def fit(self, x_train, y_train):
+        """根据训练集x_train, y_train训练Simple Linear Regression模型"""
+        assert x_train.ndim == 1, \
+            "Simple Linear Regressor can only solve single feature trainning data."
+        assert len(x_train) == len(y_train), \
+            "the size of x_train must be equal to the size of y_train."
+
+        x_mean = np.mean(x_train)
+        y_mean = np.mean(y_train)
+
+        num = 0.0
+        d = 0.0
+
+        num = (x_train - x_mean).dot(y_train - y_mean)
+        d = (x_train - x_mean).dot(x_train - x_mean)
+
+        self.a_ = num / d
+        self.b_ = y_mean - self.a_ * x_mean
+
+        return self
+
+    def predict(self, x_predict):
+        """给定待预测数据集x_predict, 返回表示x_predict的结果向量"""
+        assert x_predict.ndim == 1, \
+            "Simple Linear Regressor can only solve single feature data."
+        assert self.a_ is not None and self.b_ is not None, \
+            "must fit before predict!"
+
+        return np.array([self._predict(x) for x in x_predict])
+
+    def _predict(self, single_x):
+        """给定单个待预测数据single_x，返回single_x的预测结果值"""
+        return single_x * self.a_ + self.b_
+
+    def __repr__(self):
+        return "SimpleLinearRegression2"
